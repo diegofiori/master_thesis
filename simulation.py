430531ec19064b750825d44ace5d86bd700f242b
Diego Fiori <diegofiori@Diegos-MacBook-Pro.local>
2019-10-11 18:40:53 +0200

    added spectral clustering

--- simulation.py
+++ simulation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import h5py
 import math
 import matplotlib.pyplot as plt
@@ -79,3 +80,183 @@ class Simulation:
 
     def time_ids(self):
         return [key for key in self.data['temperature'].keys() if key[0].isdigit()]
+
+    @staticmethod
+    def _compute_perp_grad(f, xs, ys):
+        """ The method computes numerically the perpendicular gradient of function f. f should be a 3D tensor with
+        dimensions (phi, x, y). The method returns a tuple with elements df/dx and df/dy.
+        Parameters
+        ----------
+        f: np.array
+            the function whose gradient is needed
+        xs: np.array
+            the x-coordinates value, corresponding to the second dimension of f, i.e. f[:, i, :] has x-coordinate xs[i]
+        ys: np.array
+            the y-coordinates value, corresponding to the second dimension of f, i.e. f[:, :, i] has y-coordinate ys[i]
+        """
+        xs = np.concatenate([xs.reshape(-1, ), np.array([0]).reshape(-1, )])
+        dx = xs - np.roll(xs, 1)
+        dx = dx[1:].reshape((1, -1, 1))
+        ys = np.concatenate([ys.reshape(-1, ), np.array([0]).reshape(-1, )])
+        dy = ys - np.roll(ys, 1)
+        dy = dy[1:].reshape((1, 1, -1))
+        n_phi, n_x, n_y = f.shape
+        new_f = np.concatenate([f, np.zeros((n_phi, 1, n_y))], axis=1)
+        dfx = new_f - np.roll(new_f, 1, axis=1)
+        new_f = np.concatenate([f, np.zeros((n_phi, n_x, 1))], axis=2)
+        dfy = new_f - np.roll(new_f, 1, axis=2)
+        return dfx[:, 1:, :]/dx, dfy[:, :, 1:]/dy
+
+    @staticmethod
+    def _compute_volume_integral(f, xs, ys, psis, R):
+        """ The method computes numerically the volume integral of the function f. f should be a 3D tensor with
+        dimensions (psi, x, y). The method returns the value of the integral.
+
+        Parameters
+        ----------
+        f: np.array
+            the function whose integral is needed
+        xs: np.array
+            the x-coordinates value, corresponding to the second dimension of f, i.e. f[:, i, :] has x-coordinate xs[i]
+        ys: np.array
+            the y-coordinates value, corresponding to the second dimension of f, i.e. f[:, :, i] has y-coordinate ys[i]
+        phis: np.array
+            the phi-coordinates value, corresponding to the second dimension of f,
+            i.e. f[i, :, :] has phi-coordinate phis[i]
+        """
+        xs = np.concatenate([xs.reshape(-1, ), np.array([xs[-1]]).reshape(-1, )])
+        # in this way we exclude the boundaries of the domain
+        dx = xs - np.roll(xs, 1)
+        dx = dx[1:].reshape((1, -1, 1))
+        ys = np.concatenate([ys.reshape(-1, ), np.array([ys[-1]]).reshape(-1, )])
+        dy = ys - np.roll(ys, 1)
+        dy = dy[1:].reshape((1, 1, -1))
+        psis = np.concatenate([psis.reshape(-1, ), np.array([psis[-1]]).reshape(-1, )])
+        dpsi = psis - np.roll(psis, 1)
+        dpsi = dpsi[1:].reshape((-1, 1, 1))
+        diff_correction = (xs + R)[:-1].reshape((1, -1, 1))
+        dx = dx * diff_correction
+        differential_prod = dx * dy * dpsi
+
+        return np.sum(f*differential_prod)
+
+    def _compute_energy_contributions(self, time_id):
+        """ The method computes the total energy and each single contribution for a given time_id.
+        The energy is supposed to be the sum of 4 different sources: the kinetic term (given by the parallel
+        ion velocity), the pressure term, the electrostatic term and the magnetic term.
+        Note that for a GBS simulation the total energy should be constant as proved in the paper by Zeiler in 1997.
+
+        Parameters
+        ----------
+        time_id: str
+            the time_id in which the energy is needed
+
+        Returns
+        -------
+        a pd.Series containing both the total energy and its contribution terms, with the following index ordering
+            ['v_pari_energy', 'pressure_energy', 'electrostatic_energy', 'magnetic_energy', 'total_energy']
+        """
+        phi = self.data['strmf'][time_id]
+        psi = self.data['psi'][time_id]
+        T_e = self.data['temperature'][time_id]
+        T_i = self.data['temperaturi'][time_id]
+        n = np.exp(self.data['theta'][time_id])
+        v_pari = self.data['vpari'][time_id]
+        # compute energy contributions
+        # v_pari
+        integrand = n * v_pari**2
+        v_pari_energy = self._compute_volume_integral(integrand, self.data['theta']['coord1'],
+                                                      self.data['theta']['coord2'],
+                                                      self.data['theta']['coord3'], R=1)
+        # pressure contribution
+        tau = 1
+        integrand = 3 * n * (T_e + tau * T_i)
+        pressure_energy = self._compute_volume_integral(integrand, self.data['temperaturi']['coord1'],
+                                                        self.data['temperaturi']['coord2'],
+                                                        self.data['temperaturi']['coord3'], R=1)
+
+        # Electrostatic contribution
+        n_0 = 1
+        temp_function = phi + n_0 * tau * n * T_i
+
+        df_dx, df_dy = self._compute_perp_grad(temp_function,
+                                               self.data['strmf']['coord1'],
+                                               self.data['strmf']['coord2'])
+        integrand = df_dx**2 + df_dy**2
+        electrostatic_energy = self._compute_volume_integral(integrand, self.data['strmf']['coord1'],
+                                                             self.data['strmf']['coord2'], self.data['strmf']['coord3'],
+                                                             R=1)
+
+        # magnetic contribution
+        dpsi_dx, dpsi_dy = self._compute_perp_grad(psi, self.data['psi']['coord1'], self.data['psi']['coord2'])
+        integrand = dpsi_dx**2 + dpsi_dy**2
+        magnetic_energy = self._compute_volume_integral(integrand, self.data['psi']['coord1'],
+                                                        self.data['psi']['coord2'], self.data['psi']['coord3'], R=1)
+
+        total_energy = v_pari_energy + pressure_energy + electrostatic_energy + magnetic_energy
+
+        return pd.Series(data=[v_pari_energy, pressure_energy, electrostatic_energy, magnetic_energy, total_energy],
+                         index=pd.Index(['v_pari_energy', 'pressure_energy', 'electrostatic_energy',
+                                         'magnetic_energy', 'total_energy']), name=time_id)
+
+    def get_energy(self):
+        """ This method produce a pandas DataFrame containing all the enrgy contribution per each time_id store inside
+        the simulation file.
+        """
+        df = pd.concat([self._compute_energy_contributions(time_id) for time_id in self.time_ids()], axis=1).transpose()
+        return df
+
+    def get_average_quantity(self, q='theta', point_coordinate=None):
+        """ This method returns a pandas Series containing the average (on psi, *axis) values for each time snapshot.
+
+        Parameters
+        ----------
+        q: str
+            the quantity that has to be monitored
+        point_coordinate: tuple, optional (default None)
+            if given the average is taken only on the psi axis, and the coordinate passed is monitored.
+        """
+        time_ids = self.time_ids()
+
+        def compute_mean(array):
+            if point_coordinate is None:
+                return float(array.mean())
+            else:
+                return array.mean(axis=0)[point_coordinate[0], point_coordinate[1]]
+
+        return pd.Series(data=[compute_mean(self.data[q][time_id]) for time_id in time_ids], index=pd.Index(time_ids))
+
+    def get_phase_space(self, time_id, coords=None):
+        """ The method gives the discretized phase space of the fluid (plasma) in the simulation. It is possible both
+        impose the coordinates axis to use or use all the information available. Since we only have the information
+        at the grid points we call phase space the space having the physical quantities as axis. Each point corresponds
+        to a grid point (x, y, psi) [or (psi, x, y) speaking with the formalism of the data]"""
+        if coords is None:
+            coords = list(set(self.data.keys()) - {'time', 'cstep'})
+        n_z, n_x, n_y = self.data[coords[0]][time_id].shape
+
+        z = self.data[coords[0]]['coord3'].reshape((-1, 1, 1))
+        x = self.data[coords[0]]['coord1'].reshape((1, -1, 1))
+        y = self.data[coords[0]]['coord2'].reshape((1, 1, -1))
+
+        def flatten_grid(array, axis_rep):
+
+            array = np.repeat(array, axis_rep[0], axis=0)
+            array = np.repeat(array, axis_rep[1], axis=1)
+            array = np.repeat(array, axis_rep[2], axis=2)
+
+            return array.flatten()
+
+        z_flat = np.expand_dims(flatten_grid(z, (1, n_x, n_y)), axis=1)
+        y_flat = np.expand_dims(flatten_grid(y, (n_z, n_x, 1)), axis=1)
+        x_flat = np.expand_dims(flatten_grid(x, (n_z, 1, n_y)), axis=1)
+        list_of_vertical_features = [z_flat, x_flat, y_flat]
+        for coord in coords:
+            list_of_vertical_features.append(np.expand_dims(self.data[coord][time_id].flatten(), axis=1))
+
+        features = np.concatenate(list_of_vertical_features, axis=1)
+
+        return features
+
+
+
--- topological_tools.py
+++ topological_tools.py
@@ -10,6 +10,8 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
+from sklearn.cluster import SpectralClustering as SCModel
+
 
 class VlasovGiotto:
     """ This class implement the features extraction from simulations using TDA. @TODO add further comments
@@ -22,7 +24,8 @@ class VlasovGiotto:
     """
     def __init__(self, simulation_path):
         self._simulation_path = simulation_path
-        self._files = [name for name in os.listdir(simulation_path) if name.split('.')[-1] == 'h5']
+        self._files = [name for name in os.listdir(simulation_path)
+                       if name.split('.')[-1] == 'h5' and name.split('_')[0] == 'results']
         # think about the possibility to pass a dictionary with the information to pass tp the constructors of
         # topological objects.
         self._filtration = DilationFiltration()
@@ -40,6 +43,10 @@ class VlasovGiotto:
         ----------
         top_q: str
             parameter which determine the type of topological extraction.
+
+        Returns
+        -------
+        a pd.DataFrame containing as indexes the times_id and as columns the topological features
         """
         for name in tqdm(self._files):
             simulation = Simulation(self._simulation_path+name)
@@ -59,6 +66,36 @@ class VlasovGiotto:
 
         return self._results
 
+    def add_physical_features(self, phys_qs=None):
+        """ The method is studied to be called after the topological features extraction. It adds to the results
+        dataframe the columns containing the energy information from the simulations.
+        """
+        if phys_qs is None:
+            phys_qs = ['theta', 'temperature', 'temperaturi', 'strmf', 'vpari', 'psi']
+        dfs = []
+        for name in tqdm(self._files):
+            simulation = Simulation(self._simulation_path+name)
+            df = simulation.get_energy()
+            for q in phys_qs:
+                df[q] = simulation.get_average_quantity(q=q)
+            dfs.append(df)
+
+        physical_df = pd.concat(dfs, axis=0)
+
+        if self._results.empty:
+            print('Attention: no precomputed topological quantities has been found. '
+                  'The code could generate some errors.')
+            self._results = physical_df
+        else:
+            self._results = pd.concat([self._results, physical_df], axis=1)
+
+        return self._results
+
+    def run(self, top_q='temperature'):
+        """ This method calls both get_topological_features and add_physical_features in the right order."""
+        self.get_topological_features(top_q)
+        return self.add_physical_features()
+
     def _extract_topology_from_simulation(self, simulation, top_q):
         """ This method extracts some topological features from the simulation."""
         df = pd.DataFrame()
@@ -66,7 +103,7 @@ class VlasovGiotto:
             angles = [0, np.pi / 4, np.pi / 2, 3 * np.pi / 4, np.pi, 5 * np.pi / 4, 3 * np.pi / 2, 7 * np.pi / 4]
             tid_diag = [self._compute_diagrams(timeid_img) for timeid_img in simulation.extract_slices(angles, top_q)]
             df_values = [self._extract_features_from_slices_diagrams(diagrams, angles) for diagrams in tid_diag]
-            df = pd.concat(df_values)
+            df = pd.concat(df_values, axis=1).transpose()
             df.index = pd.Index(simulation.time_ids())
         else:
             TypeError(f'The type {top_q} is not supported')
@@ -85,9 +122,11 @@ class VlasovGiotto:
         for key in diagrams.keys():
             keys += ['sup_' + str(key) + '_' + str(angle) for angle in angles]
             values.append(diagrams[key][:, :, 1].max(axis=1).reshape(-1, ))
-            keys += ['inf_' + str(key) + str(angle) for angle in angles]
+            keys += ['inf_' + str(key) + '_' + str(angle) for angle in angles]
             values.append(diagrams[key][:, :, 1].min(axis=1).reshape(-1, ))
             # add some other key-dependent quantities?
+
+        diagrams = self._add_zero_to_diagrams(diagrams)
 
         for metric in metrics:
             keys += [metric + '_' + str(angle) for angle in angles]
@@ -128,11 +167,13 @@ class VlasovGiotto:
         for key in diagrams.keys():
             _, y, z = diagrams[key].shape
             zero_diagram = np.zeros((1, y, z))
-            diagrams[key] = np.concatenate(zero_diagram, diagrams[key])
+            diagrams[key] = np.concatenate([zero_diagram, diagrams[key]])
         return diagrams
 
-    def _compute_diagrams_distance_from_zero(self, diagrams, metric):
-        """ The method computes the distance for each diagram from the 'zero' using the specified metric.
+    @staticmethod
+    def _compute_diagrams_distance_from_zero(diagrams, metric):
+        """ The method computes the distance for each diagram from the 'zero' using the specified metric. The 'zero'
+        diagram is supposed to be the first diagram.
 
         Parameters
         ----------
@@ -141,8 +182,63 @@ class VlasovGiotto:
             the metric to be used to compute the distance between diagrams
         """
         distance = DiagramDistance(metric=metric)
-        diagrams = self._add_zero_to_diagrams(diagrams)
         distance_array = distance.fit_transform(diagrams)[1:, 0]
         return distance_array
+
+    def _spectral_clustering(self, simulation):
+        time_ids = simulation.time_ids()
+        model = SpectralClustering()
+        list_of_eigenvalues = [model.get_eigenvalues(simulation.get_phase_space(time_id)) for time_id in time_ids]
+        return pd.Series(data=list_of_eigenvalues, index=pd.Index(time_ids))
+
+
+class SpectralClustering:
+    """ The class implement the spectral clustering method. Note that the Scikit-learn class has not been used since
+    it does not provide the graph Laplacian eigenvalues and eigenvectors.
+    """
+    def __init__(self, n_neighbors=5, affinity='rbf'):
+        self._n_neighbors = n_neighbors
+        self._affinity = affinity
+        self._eigenvalues = None
+        self._adjency = None
+
+    def fit(self, X):
+        # compute the graph
+        spectral_clustering_model = SCModel(affinity=self._affinity, n_neighbors=self._n_neighbors)
+        spectral_clustering_model.fit(X)
+        adjency = spectral_clustering_model.affinity_matrix_
+        self._adjency = adjency
+
+        # compute the Graph Laplacian
+        degree_matrix = np.diag(adjency.sum(axis=1))
+        laplacian = degree_matrix - adjency
+
+        # Now we can compute the eigenvalues and the eigenvectors
+        eig_val, _ = np.linalg.eig(laplacian)
+
+        self._eigenvalues = eig_val
+
+        return self
+
+    def predict(self, X, n_clusters=8):
+        spectral_clustering_model = SCModel(n_clusters=n_clusters, affinity='precomputed')
+        labels = spectral_clustering_model.fit_predict(self._adjency)
+        return labels
+
+    @property
+    def eigenvalues(self):
+        return np.sort(self._eigenvalues)
+
+    def get_eigenvalues(self, X):
+        self.fit(X)
+        return self.eigenvalues
+
+
+
+
+
+
+
+
 
 
