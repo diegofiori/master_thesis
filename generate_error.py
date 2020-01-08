ff248621a76f34eba559bafb17f12f740695cc51
Diego Fiori <diegofiori@Diegos-MacBook-Pro.local>
2020-01-08 16:08:13 +0100

    video maker implemented

---
+++ generate_error.py
@@ -0,0 +1,27 @@
+import yaml
+from joblib import Parallel, delayed
+import numpy as np
+
+from input_reader import get_all_time_ids
+from run import process_images_data
+
+FIELDS = ['theta']
+
+if __name__=='__main__':
+    with open("config.yml", 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
+    print(cfg)
+    simulation_path = cfg['data_path']['simulation_path']
+    save_path = cfg['data_path']['save_path']
+    n_time_steps = cfg['inputs']['n_time_steps']
+    n_jobs = cfg['inputs']['n_jobs']
+    resample_period = cfg['inputs']['resample_period']
+    time_ids = get_all_time_ids(simulation_path)
+
+    features = Parallel(n_jobs=n_jobs)(delayed(process_images_data)(simulation_path, FIELDS[i],
+                                                                    time_ids[j:min(j+n_time_steps, len(time_ids))],
+                                                                    80)
+                                       for j in range(0, len(time_ids), n_time_steps)
+                                       for i in range(len(FIELDS)))
+    features = [np.array(features[i:i+len(FIELDS)]) for i in range(0, len(features)-len(FIELDS), len(FIELDS))]
+    features = np.concatenate(features, axis=1)
